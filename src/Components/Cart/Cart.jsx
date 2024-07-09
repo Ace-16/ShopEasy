7693bd48a201e0c1e6a6a03f490f1782ac5378b3
Ace-16 <samdeshmukh300@gmail.com>
2024-07-09 19:57:03 +0530

    Debuuging: Error fixing for the css styling

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import FlipMove from "react-flip-move";
+
 import "./Cart.css";
 import Subtotal from "../Subtotal/Subtotal.jsx";
 import CartProduct from "../CartProduct/CartProduct.jsx";
 import { useStateValue } from "../../StateProvider.jsx";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
   const [{ basket, user }] = useStateValue();
@@ -13,19 +14,22 @@ const Cart = () => {
       <div className="cart__left">
         <div>
           {user ? <h3>Hello, {user?.email}</h3> : ""}
-          <h1 className="cart__title">Your shopping basket</h1>
-          <FlipMove>
-            {basket.map((item, index) => (
-              <CartProduct
-                key={`${item.id}-${index}`}
-                id={item.id}
-                title={item.title}
-                image={item.image}
-                price={item.price}
-                rating={item.rating}
-              />
-            ))}
-          </FlipMove>
+          <h1 className="cart__title">
+            Your shopping basket{basket.length === 0 ? " is Empty" : ""}
+          </h1>
+          {basket.map((item, index) => (
+            <CartProduct
+              key={`${item.id}-${index}`}
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+            />
+          ))}
+          <Link to={"/"}>
+            <button className="cart__addons">Add more items?</button>
+          </Link>
         </div>
       </div>
       <div className="cart__right">
